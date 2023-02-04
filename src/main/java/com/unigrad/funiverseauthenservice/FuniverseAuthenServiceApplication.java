b59a7bf18a7255c0bfe914fc310b0059706349e1
uyentrang2247 <truongthiuyentrang@gmail.com>
2023-02-04 12:27:07 +0700

    first commit

---
+++ src/main/java/com/unigrad/funiverseauthenservice/FuniverseAuthenServiceApplication.java
@@ -0,0 +1,13 @@
+package com.unigrad.funiverseauthenservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuniverseAuthenServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuniverseAuthenServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unigrad/funiverseauthenservice/FuniverseAuthenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unigrad.funiverseauthenservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuniverseAuthenServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
